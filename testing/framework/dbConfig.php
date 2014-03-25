dc30a3fa48b1d10f088cccef9616f3e3e07df760
edasubert <edasubert@gmail.com>
2014-03-25 09:53:03 +0100

    security fix (exposed details)

--- testing/framework/dbConfig.php
+++ testing/framework/dbConfig.php
@@ -1,6 +1,6 @@
 <?php
-	define('HOST', 'localhost' );
-	define('USERNAME', 'eduard' );
-	define('PASSWORD', 'Sc7Ab3AUB784qdSY' );
-	define('DBNAME', 'test' );
+	define('HOST', 'host' );
+	define('USERNAME', 'username' );
+	define('PASSWORD', 'password' );
+	define('DBNAME', 'dbnam' );
 ?>
